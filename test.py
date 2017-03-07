23452cf9159e5e06f479318af9edcd2e0e2abed6
jhao104 <jinghao@bbdservice.com>
2017-03-07 11:21:31 +0800

    [add] add test

---
+++ test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     test.py  
+   Description :  
+   Author :       JHao
+   date：          2017/3/7
+-------------------------------------------------
+   Change Activity:
+                   2017/3/7: 
+-------------------------------------------------
+"""
+__author__ = 'JHao'
+
+from Schedule import ProxyRefreshSchedule
