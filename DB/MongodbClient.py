56dc2fe421147bcea31acd2f568820a637502f13
netAir <netair@netair.xyz>
2017-09-26 16:12:30 +0800

    无耻的在作者版权信息上加上我的名,嘿嘿

--- DB/MongodbClient.py
+++ DB/MongodbClient.py
@@ -1,10 +1,19 @@
 # coding: utf-8
-
-__author__ = 'Maps'
+"""
+-------------------------------------------------
+   File Name：    MongodbClient.py
+   Description :  封装mongodb操作
+   Author :       JHao netAir
+   date：          2017/3/3
+-------------------------------------------------
+   Change Activity:
+                   2017/3/3:
+                   2017/9/26:完成对mongodb的支持
+-------------------------------------------------
+"""
+__author__ = 'Maps netAir'
 
 from pymongo import MongoClient
-import random
-import json
 
 
 class MongodbClient(object):
