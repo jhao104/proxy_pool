663788cccf3758a5d637d642256ce6a39f87f958
highroom <827148@163.com>
2018-04-05 14:32:44 +0800

    modify proxy valid use queue

--- Schedule/ProxyValidSchedule.py
+++ Schedule/ProxyValidSchedule.py
@@ -17,11 +17,15 @@ import sys
 sys.path.append('../')
 
 from Schedule.ProxyCheck import ProxyCheck
+from Manager.ProxyManager import ProxyManager
+from queue import Queue
+import time
 
 
-class ProxyValidSchedule(object):
+class ProxyValidSchedule(ProxyManager, object):
     def __init__(self):
-        pass
+        ProxyManager.__init__(self)
+        self.queue = Queue()
 
     def __validProxy(self, threads=5):
         """
@@ -31,7 +35,7 @@ class ProxyValidSchedule(object):
         """
         thread_list = list()
         for index in range(threads):
-            thread_list.append(ProxyCheck())
+            thread_list.append(ProxyCheck(self.queue, self.item_dict))
 
         for thread in thread_list:
             thread.daemon = True
@@ -41,7 +45,20 @@ class ProxyValidSchedule(object):
             thread.join()
 
     def main(self):
-        self.__validProxy()
+        self.put_queue()
+        while True:
+            if self.queue.qsize():
+                self.__validProxy()
+            else:
+                print('Time sleep 5 minutes.')
+                time.sleep(60 * 5)
+                self.put_queue()
+
+    def put_queue(self):
+        self.db.changeTable(self.useful_proxy_queue)
+        self.item_dict = self.db.getAll()
+        for item in self.item_dict:
+            self.queue.put(item)
 
 
 def run():
