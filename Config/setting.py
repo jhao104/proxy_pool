bb4a7b9367a74645d1bfecbf92299260ef4bde0f
houbaron <houbaron@gmail.com>
2019-05-08 21:44:56 +0800

    [refine]设置默认密码

--- Config/setting.py
+++ Config/setting.py
@@ -24,11 +24,11 @@ DB_TYPE = getenv('db_type', 'SSDB')
 if DB_TYPE == 'SSDB':
     DB_HOST = getenv('ssdb_host', '127.0.0.1')
     DB_PORT = getenv('ssdb_port', '6379')
-    DB_PASSWORD = getenv('ssdb_password', '6379')
+    DB_PASSWORD = getenv('ssdb_password', '')
 elif DB_TYPE == 'MONGODB':
     DB_HOST = getenv('mongodb_host', '127.0.0.1')
     DB_PORT = getenv('mongodb_host', '27017')
-    DB_PASSWORD = getenv('mongodb_password', '6379')
+    DB_PASSWORD = getenv('mongodb_password', '')
 else:
     raise ConfigError('Unknown database type, your environment variable `db_type` should be one of SSDB/MONGODB.')
 
