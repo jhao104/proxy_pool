90608d88a41a248f4380c05931fc3292a9f39564
jhao <jingh@jccfc.com>
2019-09-11 10:50:00 +0800

    [update] ckeckproxy

--- Util/utilFunction.py
+++ Util/utilFunction.py
@@ -83,14 +83,13 @@ def validUsefulProxy(proxy):
     :return:
     """
     if isinstance(proxy, bytes):
-        proxy = proxy.decode('utf8')
+        proxy = proxy.decode("utf8")
     proxies = {"http": "http://{proxy}".format(proxy=proxy)}
     try:
-        # 超过20秒的代理就不要了
-        r = requests.get('http://httpbin.org/ip', proxies=proxies, timeout=10, verify=False)
-        if r.status_code == 200 and r.json().get("origin"):
-            # logger.info('%s is ok' % proxy)
+        r = requests.get('http://www.baidu.com', proxies=proxies, timeout=10, verify=False)
+        if r.status_code == 200:
             return True
     except Exception as e:
-        # logger.error(str(e))
-        return False
+        pass
+    return False
+
