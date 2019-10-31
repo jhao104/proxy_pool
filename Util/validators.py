b615493d947df1c47cf9a77bc6e9c758268ebd28
康鑫 <k506855660@163.com>
2019-10-31 09:41:43 +0800

    解决冲突

--- Util/validators.py
+++ Util/validators.py
@@ -16,17 +16,15 @@ def timeOutValidator(proxy):
     :return:
     """
     if isinstance(proxy, bytes):
-        proxy = proxy.decode('utf8')
+        proxy = proxy.decode("utf8")
     proxies = {"http": "http://{proxy}".format(proxy=proxy)}
     try:
-        # 超过10秒的代理就不要了
-        r = requests.get('http://httpbin.org/ip', proxies=proxies, timeout=10, verify=False)
-        if r.status_code == 200 and r.json().get("origin"):
-            # logger.info('%s is ok' % proxy)
+        r = requests.get('http://www.baidu.com', proxies=proxies, timeout=10, verify=False)
+        if r.status_code == 200:
             return True
     except Exception as e:
-        # logger.error(str(e))
-        return False
+        pass
+    return False
 
 
 @validator
