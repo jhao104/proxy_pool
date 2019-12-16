89cec7e45d2a5f8513c7063fd904dcddae90d6ba
p9s <mc_cheung@126.com>
2019-12-16 23:13:05 +0800

    new proxy list

--- ProxyGetter/getFreeProxy.py
+++ ProxyGetter/getFreeProxy.py
@@ -323,6 +323,27 @@ class GetFreeProxy(object):
             for proxy in proxies:
                 yield ':'.join(proxy)
 
+    @staticmethod
+    def freeProxy15(max_page=1):
+        """
+        https://proxyranker.com/
+        89免费代理
+        :param max_page:
+        :return:
+        """
+        base_url = 'https://proxyranker.com/'
+        request = WebRequest()
+        for page in range(1, max_page + 1):
+            url = base_url.format(page)
+            r = request.get(url, timeout=10)
+            proxies = re.findall(
+                #r'<td.*?>[\s\S]*?(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})[\s\S]*?</td>[\s\S]*?<td.*?>[\s\S]*?(\d+)[\s\S]*?</td>',
+                #r'<td.*?>[\s\S]*?(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})[\s\S]*?</td>[\s\S]*?<td.*?>[\s\S]*?(\d+)[\s\S]*?</td>',
+		r'<td>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})</td><td>.*?</td><td>.*?</td>[\s\S]*?<td><span title="Proxy port">(\d+?)</span></td>',
+                r.text)
+            for proxy in proxies:
+                yield ':'.join(proxy)
+
 
 if __name__ == '__main__':
     from CheckProxy import CheckProxy
