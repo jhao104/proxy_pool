56c677c316403f129b17271072187bbce8bad630
jhao <jingh@jccfc.com>
2020-06-23 10:54:06 +0800

    [update] configHandler.py

---
+++ handler/configHandler.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     configHandler
+   Description :
+   Author :        JHao
+   date：          2020/6/22
+-------------------------------------------------
+   Change Activity:
+                   2020/6/22:
+-------------------------------------------------
+"""
+__author__ = 'JHao'
+
+import os
+import setting
+from util.singleton import Singleton
+from util.lazyProperty import LazyProperty
+
+
+class ConfigHandler(object):
+    __metaclass__ = Singleton
+
+    def __init__(self):
+        pass
+
+    @LazyProperty
+    def serverHost(self):
+        return os.environ.get("HOST", setting.HOST)
+
+    @LazyProperty
+    def serverPort(self):
+        return os.environ.get("PORT", setting.PORT)
+
+    @LazyProperty
+    def dbConn(self):
+        return os.getenv("DB_CONN", setting.DB_CONN)
