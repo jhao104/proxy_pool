a31814edca3918f38b1b03e0900a69d3648b9d81
jhao <jingh@jccfc.com>
2020-07-06 17:30:22 +0800

    [del] :heavy_check_mark: restruct

--- util/__init__.py
+++ util/__init__.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 """
 -------------------------------------------------
-   File Name：     __init__.py.py  
-   Description :  
-   Author :       JHao
-   date：          2016/11/25
+   File Name：     __init__
+   Description :
+   Author :        JHao
+   date：          2020/7/6
 -------------------------------------------------
    Change Activity:
-                   2016/11/25: 
+                   2020/7/6:
 -------------------------------------------------
 """
-
-# from util.utilFunction import validUsefulProxy
-# from util.LogHandler import LogHandler
-# from util.utilClass import Singleton
+__author__ = 'JHao'
