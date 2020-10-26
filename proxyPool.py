c5bd6dce62b420569a6af54d634e8464bac611e4
jhao <jingh@jccfc.com>
2020-10-26 10:05:18 +0800

    [update] 2.1.1

--- proxyPool.py
+++ proxyPool.py
@@ -20,7 +20,7 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
 @click.group(context_settings=CONTEXT_SETTINGS)
-@click.version_option(version='2.1.0')
+@click.version_option(version='2.1.1')
 def cli():
     """ProxyPool cli工具"""
 
