c5f83092278af114037ca4d1627a0765d26a5bb6
nansirc <654344371@qq.com>
2021-02-07 09:54:46 +0800

    update test file

---
+++ test/testIsHttps.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     testIsHttps
+   Description :
+   Author :        nansircroft
+   date：          2021/2/06
+-------------------------------------------------
+   Change Activity:
+                   2020/2/06:
+-------------------------------------------------
+"""
+__author__ = 'nansircroft'
+
+from helper.ishttps import runHttpsChecker
+
+def testIsHttps():
+    runHttpsChecker()
+
+
+
+if __name__ == "__main__":
+    testIsHttps()
